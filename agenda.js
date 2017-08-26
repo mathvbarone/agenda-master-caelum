78d5bff5e250ffb9a6b1df012f52f9ab8a0162fb
Matheus Barone <matheusvbarone@gmail.com>
2017-08-26 16:56:05 -0300

    Adicionando evento no botão

--- agenda.js
+++ agenda.js
@@ -1,16 +1,31 @@
 (function(){
-  //DECLARANDO OS CAMPOS DE INTERACAO DO USUARIO
+  /******DECLARANDO OS CAMPOS DE INTERACAO DO USUARIO******/
   var ui = {
     fields: document.querySelectorAll("input"),
     button: document.querySelector("button"),
     table: document.querySelector("table")
   };
 
-  //DECLARANDO AS FUNÇÕES DE AÇÃO
-  var validateFields = function(){};
+  /******DECLARANDO AS FUNÇÕES DE AÇÃO******/
+
+  //CAMPOS DE VALIDAÇÃO
+  var validateFields = function(e){
+    console.log("Validação",e);
+    e.preventDefault();
+  };
+
+  //SALVAR INFORMACAO
   var saveData = function(){};
+
+  // LISTAS OS ITENS
   var listAll = function(){};
 
-  console.log(ui);
+
+  // CRIANDO FUNÇÃO DE INICIAÇÃO
+  var initialize = function(){
+    //MAPEANDO OS EVENTOS
+    ui.button.addEventListener("click", validateFields);
+
+  }();
 
 })();

a8d3d165606d2c93647c1f5d8948e32b5676413a
Matheus Barone <matheusvbarone@gmail.com>
2017-08-26 15:42:31 -0300

    Criando a estrutura básica de Javascript

--- agenda.js
+++ agenda.js
@@ -1,1 +1,16 @@
-function(){}
+(function(){
+  console.log("Schedule");
+
+  var ui = {
+    fields: null,
+    button: null,
+    table: null
+  };
+
+  var validateFields = function(){};
+  var saveData = function(){};
+  var listAll = function(){};
+
+  console.log(ui);
+
+})();

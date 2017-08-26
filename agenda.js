fe50a8836623df5bb1007b6f11e2d8f51665a1bc
Matheus Barone <matheusvbarone@gmail.com>
2017-08-26 15:59:53 -0300

    Selecionando os elementos atraves do document.querySelector

--- agenda.js
+++ agenda.js
@@ -1,12 +1,12 @@
 (function(){
-  console.log("Schedule");
-
+  //DECLARANDO OS CAMPOS DE INTERACAO DO USUARIO
   var ui = {
-    fields: null,
-    button: null,
-    table: null
+    fields: document.querySelectorAll("input"),
+    button: document.querySelector("button"),
+    table: document.querySelector("table")
   };
 
+  //DECLARANDO AS FUNÇÕES DE AÇÃO
   var validateFields = function(){};
   var saveData = function(){};
   var listAll = function(){};

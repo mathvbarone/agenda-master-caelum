d6dc008e04f7072d5ca4d77aa1b4bd6279fa0c9d
Matheus Barone <matheusvbarone@gmail.com>
2017-09-02 12:28:04 -0300

    Configurando a requisição via Fetch

--- agenda.js
+++ agenda.js
@@ -13,27 +13,47 @@
     e.preventDefault();
     var erros = 0;
     var data = {};
+
+    //FUNÇÃO DE VERIFICAÇÃO DE PREENCHIMENTO DOS CAMPOS
     ui.fields.forEach(function(field){
-      // console.log(field.value, field.value.trim().length);
+      // SE TIVER VAZIO, MOSTRA A MENSAGEM DE ERRO
       if(field.value.trim().length === 0){
         field.classList.add("error");
         erros++;
-      }else{
+      }
+      // SE TIVER PREENCHIDO, REMOVE O ERRO DO CAMPO
+      else{
         field.classList.remove("error");
-        // console.log(field.id, field.value);
         data[field.id]=field.value;
       }
     });
+
+    //SE TIVER ERRO, DA FOCUS NO CAMPO QUE PRECISA SER PREENCHIDO
     if(erros > 0){
       document.querySelector(".error").focus();
-    }else{
+    }
+    //SE NÃO TIVER, ENVIA AS INFORMAÇÕES PARA A FUNÇÃO QUE SALVA INFORMAÇÕES
+    else{
       saveData(data);
     }
   };
 
-  //SALVAR INFORMACAO
-  var saveData = function(){
-    debugger;
+  //FUNÇÃO QUE FAZ A REQUISIÇÃO, E SALVA AS INFORMAÇÕES
+  var saveData = function(contact){
+    //CRIANDO O HEADER
+    var headers = new Headers();
+    headers.append("Content-type", "application/json");
+    //CONFIGURANDO O HEADER
+    var conf = {
+      method: "POST",
+      body: JSON.stringify(contact),
+      headers: headers
+    }
+    // FAZENDO A REQUISIÇÃO
+    fetch("http://localhost:3000/contacts", conf)
+
+
+
   };
 
   // LISTAS OS ITENS
@@ -48,3 +68,17 @@
   }();
 
 })();
+
+
+
+
+
+// REST API
+
+// ENDPOINT (Endereco que vai bater pra gerir os dados) - http://localhost:3030/contacts
+
+// CRIAR - POST
+// ATUALIZAR TODAS AS INFORMAÇÕES - PUT
+// ATUALIZAR INFORMACOES ESPECIFICAS - PATCH
+// REMOVER - DELETE
+// LISTAR - GET

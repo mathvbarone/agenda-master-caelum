88dd810805ee130cafef4b4f8d71db6a86b7b70d
Matheus Barone <matheusvbarone@gmail.com>
2017-09-02 10:20:11 -0300

    Fazendo a função de validação dos campos

--- agenda.js
+++ agenda.js
@@ -10,12 +10,31 @@
 
   //CAMPOS DE VALIDAÇÃO
   var validateFields = function(e){
-    console.log("Validação",e);
     e.preventDefault();
+    var erros = 0;
+    var data = {};
+    ui.fields.forEach(function(field){
+      // console.log(field.value, field.value.trim().length);
+      if(field.value.trim().length === 0){
+        field.classList.add("error");
+        erros++;
+      }else{
+        field.classList.remove("error");
+        // console.log(field.id, field.value);
+        data[field.id]=field.value;
+      }
+    });
+    if(erros > 0){
+      document.querySelector(".error").focus();
+    }else{
+      saveData(data);
+    }
   };
 
   //SALVAR INFORMACAO
-  var saveData = function(){};
+  var saveData = function(){
+    debugger;
+  };
 
   // LISTAS OS ITENS
   var listAll = function(){};
